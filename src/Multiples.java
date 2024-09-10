12ae53063b3092b0214cb60515593b8ef9860a58
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-09-10 14:19:51 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        for (int k = 1; k < 1001; k++) {
+            boolean divisibleby3 = k % 3 == 0;
+            boolean divisibleby5 = k % 5 == 0;
+            if (divisibleby3) {
+                i++;
+            }
+            if (divisibleby5) {
+                j++;
+            }
+        }
+        System.out.println(i);
+        System.out.println(j);
+    }
 }
